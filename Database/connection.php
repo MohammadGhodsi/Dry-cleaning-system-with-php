e73156101a47aad1e8efd2c381e5135caf36afa0
Mohammad Ghodsi <gsf350mail@gmail.com>
2021-08-30 00:25:51 +0430

    Update connection.php

--- Database/connection.php
+++ Database/connection.php
@@ -5,6 +5,19 @@ class Connection {
     protected $db;
     protected $transaction;
 
+    public function __construct($username="root" , $password="" , $host="localhost" ,$dbname="laundry" ,$options = [])
+
+    {
+        $this->isconnected = true;
+        try 
+        {
+            $this->db = new PDO("mysql:host={$host}; , dbname={$dbname}; charset=utf8" , $username , $password , $options);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+            $this->transaction = $this->db;
+            $this->db->setAttribute(PDO::attr_def)
+        }   
+    }
+
 
 
 
